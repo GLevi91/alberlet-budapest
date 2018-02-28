a77eced525830d632fa3244bb18952da9e77af4e
Gombos <levente.gombos@sap.com>
2018-02-28 16:00:15 +0100

    Created basic structure of data insert

--- model/server.js
+++ model/server.js
@@ -1,8 +1,23 @@
 var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 
-var uri = "mongodb+srv://GLevi91:Mancity917@budapestalberletcluster-p3bbk.mongodb.net/alberlet";
-MongoClient.connect(uri, function(err, client) {
-    const collection = client.db("alberlet").collection("lakasok");
-    // perform actions on the collection object
-    client.close();
-});
+function insertData (inputData){
+    const url = "mongodb+srv://GLevi91:Mancity917@budapestalberletcluster-p3bbk.mongodb.net/alberlet";
+    MongoClient.connect(url)
+    .then(function(client){
+        const collection = client.db("alberlet").collection("lakasok");
+        return new Promise(function(resolve, reject){
+            resolve(collection);
+        });
+    })
+    .then(function(collection){
+        var result = collection.insertOne(inputData);
+        return result;
+    })
+    .then(function(result){
+        assert.equal(1, result.result.n);
+        console.log("Inserted 1 document into the collection");
+    })
+}
+
+module.exports = insertData();
